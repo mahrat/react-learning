cb778a3cc0dfa8207880e591b9656965d9cffaab
mahrat <career.maheshraturi@gmail.com>
2024-03-10 08:28:12 +0530

    added css to profile cards

--- profile-card-app/src/App.js
+++ profile-card-app/src/App.js
@@ -1,13 +1,34 @@
+import 'bulma/css/bulma.css';
 import ProfileCard from './ProfileCard';
+import AlexaImage from './images/alexa.png';
+import CortanaImage from './images/cortana.png';
+import SiriImage from './images/siri.png';
 
 
-function App(){
+function App() {
     return (
         <div>
-            <div>Personal Digital Assistants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
+            <section className="hero is-primary">
+                <div className="hero-body">
+                    <p className="title">Personal Digital Assistants</p>
+                </div>
+            </section>
+
+            <div className="container">
+                <section className="section">
+                    <div className="columns">
+                        <div className="column is-4">
+                            <ProfileCard title="Alexa" handle="@alexa99" image={{ src: AlexaImage, alt: "alexa" }} />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Cortana" handle="@cortana32" image={{ src: CortanaImage, alt: "cortana" }} />
+                        </div>
+                        <div className="column is-4">
+                            <ProfileCard title="Siri" handle="@siri22" image={{ src: SiriImage, alt: "siri" }} />
+                        </div>
+                    </div>
+                </section>
+            </div>
         </div>
     );
 }
--- profile-card-app/src/ProfileCard.js
+++ profile-card-app/src/ProfileCard.js
@@ -1,5 +1,26 @@
-function ProfileCard(){
-    return <h5>Profile Card!!</h5>;
+function ProfileCard({ title, handle, image }) {
+    return (
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image.src} alt={image.alt} />
+                </figure>
+            </div>
+            <div className="card-content">
+                <div className="media">
+                    <div className="media-left">
+                        <figure className="image is-48x48">
+                            <img src={image.src} alt={image.alt} />
+                        </figure>
+                    </div>
+                    <div className="media-content">
+                        <p className="title is-4">{title}</p>
+                        <p className="subtitle is-6">{handle}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default ProfileCard;
